5eee8f5d9544998485ae5bc405e91f3f3d5aedca
Yursksf1 <Yurs.Ksf1@gmail.com>
2021-06-18 09:33:15 -0500

    Update local_data.py

--- local_data.py
+++ local_data.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 information = {
-    'username': 'zirusdigital@gmail.com',
-    'password': 'ZPDigital*'
+    'username': 'mi_email_sender@mail.com',
+    'password': 'm1_$upper_P4ssW0d!*'
 }
 
 
